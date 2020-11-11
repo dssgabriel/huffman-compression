3c335432015ad9690cc76913c2f3237b1ee2e4df
dssgabriel <gabriel.ds78@gmail.com>
2020-11-11 23:34:06 +0100

    Implemented constructors, destructor and operator= overload

--- src/lib/ArbreB.cpp
+++ src/lib/ArbreB.cpp
@@ -1,1 +1,65 @@
 #include "ArbreB.hpp"
+
+ArbreB::ArbreB()
+    : m_Root(nullptr)
+{
+}
+
+ArbreB::ArbreB(const char& data, const double& freq)
+{
+    m_Root = new Sommet(data, freq);
+}
+
+ArbreB::ArbreB(const Sommet& node)
+{
+    m_Root = new Sommet();
+    *m_Root = node;
+}
+
+ArbreB::ArbreB(const ArbreB& other)
+{
+    *this = other;
+}
+
+ArbreB::~ArbreB()
+{
+    if (m_Root != nullptr)
+    {
+        delete m_Root;
+    }
+}
+
+ArbreB& ArbreB::operator=(const ArbreB& other)
+{
+    if (other.m_Root != nullptr)
+    {
+        m_Root = new Sommet();
+        *m_Root = *other.m_Root;
+    }
+    return *this;
+}
+
+Sommet& ArbreB::get_m_Root()
+{
+    return *m_Root;
+}
+
+void ArbreB::insert(const Sommet& node, const double& freq)
+{
+    if (m_Root == nullptr)
+    {
+        *m_Root = node;
+    }
+}
+
+bool ArbreB::search(const double& freq)
+{
+}
+
+ArbreB& ArbreB::remove(const double& freq)
+{
+}
+
+ArbreB ArbreB::fuse(const ArbreB& other)
+{
+}
--- src/lib/ArbreB.hpp
+++ src/lib/ArbreB.hpp
@@ -10,9 +10,20 @@ private:
 
 public:
     ArbreB();
-    ArbreB(Sommet& sommet);
+    ArbreB(const char& data, const double& freq);
+    ArbreB(const Sommet& node);
     ArbreB(const ArbreB& other);
     ~ArbreB();
+
+    ArbreB& operator=(const ArbreB& other);
+
+    Sommet& get_m_Root();
+
+    void insert(const Sommet& node, const double& freq);
+    bool search(const double& freq);
+    ArbreB& remove(const double& freq);
+
+    ArbreB fuse(const ArbreB& other);
 };
 
 #endif
