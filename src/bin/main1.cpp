b7b595f1ed8e3008d9b0a64b825edaf101d6a19d
dssgabriel <gabriel.ds78@gmail.com>
2020-11-17 18:44:13 +0100

    Added missing tests and tweaked term output

--- src/bin/main1.cpp
+++ src/bin/main1.cpp
@@ -91,8 +91,15 @@ int main() {
         std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
     }
 
-    std::cout << "Test for inserting Sommets:\t\t";
+    std::cout << "Test for inserting Sommet:\t\t";
     if (test.should_insert_Sommet_into_ArbreB() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for inserting existing Sommet:\t";
+    if (test.should_update_freq_if_char_already_in_ArbreB() == true) {
         std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
     } else {
         std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
@@ -112,8 +119,50 @@ int main() {
         std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
     }
 
-    std::cout << "Test for removing a Sommet:\t\t";
+    std::cout << "Test for removing Sommet (leaf):\t";
     if (test.should_remove_leaf() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for removing Sommet (1 child):\t";
+    if (test.should_remove_Sommet_with_one_child() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for removing Sommet (2 children):\t";
+    if (test.should_remove_Sommet_with_two_children() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for fusing two ArbreBs:\t\t";
+    if (test.should_fuse_two_ArbreB() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for decomposing one ArbreB:\t";
+    if (test.should_decompose_one_ArbreB_into_two() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for finding a character (BFS):\t";
+    if (test.should_find_character_y_with_bfs() == true) {
+        std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
+    }
+
+    std::cout << "Test for not finding a character (BFS):\t";
+    if (test.should_not_find_character_s_with_bfs() == true) {
         std::cout << "\e[1;32mPASSED!\e[0m" << std::endl;
     } else {
         std::cout << "\e[1;31mFAILED!\e[0m" << std::endl;
@@ -123,7 +172,13 @@ int main() {
     std::cout << "\n\e[1mTests run: \e[32m" << test.tests_run
               << "\e[0m\e[1m,  Tests failed: \e[31m" << test.tests_failed
               << "\e[0m\e[1m,  Tests skipped: \e[34m" << skipped_tests
-              << "\n\e]0m" << std::endl;
+              << "\e[0m" << std::endl;
+
+    if (test.tests_failed == 0) {
+        std::cout << "\e[1mStatus: \e[32mBUILD SUCCESS\e[0m" << std::endl;
+    } else {
+        std::cout << "\e[1mStatus: \e[31mBUILD FAILURE\e[0m" << std::endl;
+    }
 
     return 0;
 }
