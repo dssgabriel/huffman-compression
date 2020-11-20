cf96e51ca2f96d1aa21e0eba8428cf40a4730773
dssgabriel <gabriel.ds78@gmail.com>
2020-11-20 22:54:28 +0100

    Added Raphael's graphical part

---
+++ graphics/main.cpp
@@ -0,0 +1,34 @@
+#include <QtGui/QApplication>
+#include <QtGui/QWidget>
+#include <QtGui/QPushButton>
+#include "MainWindow.hpp"
+
+
+
+int main(int argc, char** argv){
+
+	QApplication app(argc, argv);
+	
+	
+	
+	MainWindow W;
+	W.show();
+	
+	
+	
+	
+	
+	
+  
+ 
+
+
+
+    return app.exec();
+}
+  
+  
+  
+
+
+
---
+++ graphics/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <QtGui/QApplication>
+#include <QtGui/QWidget>
+#include <QtGui/QPushButton>
+#include "MainWindow.hpp"
+
+
+int main(int argc, char** argv){
+
+	QApplication app(argc, argv);
+	
+	MainWindow W;
+	W.show();
+	
+	
+	return app.exec();
+}
+
+
